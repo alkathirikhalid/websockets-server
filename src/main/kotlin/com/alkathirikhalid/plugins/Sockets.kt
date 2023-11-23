5c74a96acc2212450ae83913687d035d304c7bb6
alkathirikhalid <khalid@alkathirikhalid.com>
2023-11-23 15:08:14 +0800

    Implement connection handling and message propagation

--- src/main/kotlin/com/alkathirikhalid/plugins/Sockets.kt
+++ src/main/kotlin/com/alkathirikhalid/plugins/Sockets.kt
@@ -1,10 +1,12 @@
 package com.alkathirikhalid.plugins
 
+import com.alkathirikhalid.Connection
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import java.time.Duration
+import java.util.*
 
 fun Application.configureSockets() {
     install(WebSockets) {
@@ -14,12 +16,26 @@ fun Application.configureSockets() {
         masking = false
     }
     routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
         webSocket("/chat") {
-            send("You are connected!")
-            for (frame in incoming) {
-                frame as? Frame.Text ?: continue
-                val receivedText = frame.readText()
-                send("You said: $receivedText")
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach {
+                        it.session.send(textWithUsername)
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
             }
         }
     }
